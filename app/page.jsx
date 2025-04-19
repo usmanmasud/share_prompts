57b1236b07db052bfdec3f94c8b3bf51f597113a
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-04-19 07:14:20 +0100

    update navbar

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,7 @@ const Home = () => {
                 <br className="max-md:hidden" />
                 <span className="orange_gradient text-center">AI-Powered Prompts</span>
             </h1>
-            <p className="desc text-center">Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts</p>
+            <p className="desc text-center">PromptAI is an open-source AI prompting tool for modern world to discover, create and share creative prompts</p>
             <Feed />
         </section>
     )
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,11 +1,132 @@
-import React from 'react'
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { signOut, getProviders, signIn, useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 
 const Nav = () => {
-    return (
-        <div>
-            nav
-        </div>
-    )
-}
+    const isUserLoggedIn = true;
+    const [providers, setProviders] = useState(null);
+    const [toggleDropdown, setToggleDropdown] = useState(false);
 
-export default Nav
+    useEffect(() => {
+        const setProviders = async () => {
+            const response = await getProviders();
+
+            setProviders(response);
+        };
+
+        setProviders();
+    }, []);
+
+    return (
+        <nav className="flex-between w-full mb-16 pt-3">
+            <Link href="/" className="flex gap-2 flex-center">
+                <Image
+                    src="/assets/images/logo.svg"
+                    width={30}
+                    height={30}
+                    className="object-contain"
+                    alt="Prompt ia"
+                />
+                <p className="logo_text">PromptAI</p>
+            </Link>
+
+            {/* Desktop */}
+            <div className="sm:flex hidden">
+                {isUserLoggedIn ? (
+                    <div className="flex gap-3 md:gap-5">
+                        <Link href="/create-promts" className="black_btn">
+                            Create Post
+                        </Link>
+                        <button type="button" onClick={signOut} className="outline_btn">
+                            Sign Out
+                        </button>
+                        <Link href="/profile">
+                            <Image
+                                src="/assets/images/logo.svg"
+                                width={37}
+                                height={37}
+                                className="rounded-full"
+                                alt="Profile"
+                            />
+                        </Link>
+                    </div>
+                ) : (
+                    <>
+                        {providers &&
+                            Object.values(providers).map((provider) => (
+                                <button
+                                    type="button"
+                                    key={provider.name}
+                                    onClick={() => signIn(provider.id)}
+                                    className="black_btn"
+                                >
+                                    Sign In
+                                </button>
+                            ))}
+                    </>
+                )}
+            </div>
+
+            {/* mobile */}
+            <div className="sm:hidden flex relative">
+                {isUserLoggedIn ? (
+                    <div className="flex">
+                        <Image
+                            src="/assets/images/logo.svg"
+                            width={37}
+                            height={37}
+                            className="rounded-full"
+                            alt="Profile"
+                            onClick={() => setToggleDropdown((prev) => !prev)}
+                        />
+                        {toggleDropdown && (
+                            <div className="dropdown">
+                                <Link
+                                    href="/profile"
+                                    className="dropdown_link"
+                                    onClick={setToggleDropdown(false)}
+                                >
+                                    My Profile
+                                </Link>
+                                <Link
+                                    href="/create-prompt"
+                                    className="dropdown_link"
+                                    onClick={setToggleDropdown(false)}
+                                >
+                                    Create Prompt
+                                </Link>
+                                <button
+                                    type="button"
+                                    onClick={() => {
+                                        setToggleDropdown(false);
+                                        signOut();
+                                    }}
+                                    className="mt-5 w-full black_btn"
+                                >Sign Out</button>
+                            </div>
+                        )}
+                    </div>
+                ) : (
+                    <>
+                        {providers &&
+                            Object.values(providers).map((provider) => (
+                                <button
+                                    type="button"
+                                    key={provider.name}
+                                    onClick={() => signIn(provider.id)}
+                                    className="black_btn"
+                                >
+                                    Sign In
+                                </button>
+                            ))}
+                    </>
+                )}
+            </div>
+        </nav>
+    );
+};
+
+export default Nav;
