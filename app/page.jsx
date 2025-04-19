e43b7e08f84a3c5437f000c75cc71633e5cb51cf
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-04-19 06:21:41 +0100

    update home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,14 @@
 
 const Home = () => {
     return (
-        <div>
-            home
-        </div>
+        <section className="w-full flex-center flex-col">
+            <h1 className="head_text text-center">Discover & share
+                <br className="max-md:hidden" />
+                <span className="orange_gradient text-center">AI-Powered Prompts</span>
+            </h1>
+            <p className="desc text-center">Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts</p>
+
+        </section>
     )
 }
 
