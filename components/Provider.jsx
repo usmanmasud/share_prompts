31943df947705d9ae89a1142428fa69254ebd519
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-04-19 07:30:28 +0100

    ..

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+"use clients"
 
-export const Provider = () => {
+import { SessionProvider } from "/next-auth/react"
+
+export const Provider = ({ children, session }) => {
     return (
-        <div>Provider</div>
+        <SessionProvider session={session}>
+            {children}
+        </SessionProvider>
     )
 }
