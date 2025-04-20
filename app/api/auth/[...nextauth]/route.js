48687def5eecf10eaa726f559ae8436bc27918c2
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-04-20 22:21:43 +0100

    update routes

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { connectToDb } from "./utils/database";
+import User from "@models/user";
 
 const handler = NextAuth({
   providers: [
@@ -10,11 +11,36 @@ const handler = NextAuth({
     }),
   ],
 
-  async session({ session }) {},
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
+
+    session.user.id = sessionUser._id.toString();
+
+    return session;
+  },
   async signIn({ profile }) {
     try {
       await connectToDb();
-    } catch (error) {}
+
+      const userExists = await User.findOne({
+        email: profile.email,
+      });
+
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
+
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
   },
 });
 
