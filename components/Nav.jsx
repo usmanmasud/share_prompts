d25c555d6c18a4d12fa1729648d4a83ffcd8e7d1
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-04-21 00:11:15 +0100

    create create prompt

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -37,7 +37,7 @@ const Nav = () => {
             <div className="sm:flex hidden">
                 {session?.user ? (
                     <div className="flex gap-3 md:gap-5">
-                        <Link href="/create-promts" className="black_btn">
+                        <Link href="/create-prompts" className="black_btn">
                             Create Post
                         </Link>
                         <button type="button" onClick={signOut} className="outline_btn">
