6e97298e0e77ffb4258218c5367928cdd5f6b4b5
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-04-28 12:42:50 +0100

    update

--- components/PromptsCard.jsx
+++ components/PromptsCard.jsx
@@ -22,10 +22,10 @@ const PromptsCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
         <div className="prompt_card">
             <div className="flex justify-center items-start gap-5">
                 <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
-                    {/* <Image src={post.creator.image} alt="user_image" width={40} height={40} className="rounded-full object-contain" /> */}
+                    <Image src={post.creator.image} alt="user_image" width={40} height={40} className="rounded-full object-contain" />
                     <div className="flex flex-col">
-                        {/* <h3 className="font-satoshi font-semibold text-gray-900">{post.creator.username}</h3>
-                        <p className="font-inter text-sm text-gray-500">{post.creator.email}</p> */}
+                        <h3 className="font-satoshi font-semibold text-gray-900">{post.creator.username}</h3>
+                        <p className="font-inter text-sm text-gray-500">{post.creator.email}</p>
                     </div>
                 </div>
                 <div className="copy_btn" onClick={handleCopy}>
